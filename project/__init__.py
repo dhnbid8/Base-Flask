4d6ebc551368d7e93ab801d4aac2d13a26bb39e4
Danial Dhnbi <dhnbid8@gmail.com>
2022-05-23 12:13:19 +0430

    move controller to project/controller instead of /project

--- project/__init__.py
+++ project/__init__.py
@@ -25,11 +25,11 @@ def create_app():
         return User.query.get(int(user_id))
         
     # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+    from .controller.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+    from .controller.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
--- project/controller/main.py
+++ project/controller/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from . import db
+from .. import db
 from flask_login import login_required, current_user
 
 
