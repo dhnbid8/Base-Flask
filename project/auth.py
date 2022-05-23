67dd4fe17664c4a4c1348b0c9b74037c8e6336a6
Danial Dhnbi <dhnbid8@gmail.com>
2022-05-23 11:40:44 +0430

    create auth model instead of models

--- project/auth.py
+++ project/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models.models import User
+from .models.authModel import User
 from flask_login import login_user, login_required, logout_user
 
 auth = Blueprint('auth', __name__)
